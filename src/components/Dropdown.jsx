691d879bbf9ee4f30836f9453f2c9ffcc811be26
Etesam913 <etesamansari13@gmail.com>
2022-08-29 18:05:41 -0400

    ♿️ Added keyboard controls

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import DropdownMenu from "./DropdownMenu";
 
 function Dropdown({
   direction = "down",
-  items = ["🦖 This is a T-Rex!", "🐶 This is a dog"],
+  items = [
+    "🦖 This is a T-Rex!",
+    "🐶 This is a dog!",
+    "🐱 This is a cat!",
+    "🍅 This is a tomato",
+  ],
   multiSelect = false,
   placeholderText = "✨ This is some placeholder text",
 }) {
@@ -13,14 +18,24 @@ function Dropdown({
   );
   const [isDropdownShowing, setIsDropdownShowing] = useState(false);
   const [selectedDropdownItems, setSelectedDropdownItems] = useState([]);
-  const dropdownWidth = "17rem";
+  const uniqueIdentifier = uuidv4();
+  const dropdownButton = useRef(null);
+
+  useEffect(() => {
+    if (isDropdownShowing) {
+      const firstDropdownElement = document.getElementById(
+        `dropdown-button-${uniqueIdentifier}-0`
+      );
+      if (firstDropdownElement) firstDropdownElement.focus();
+    }
+  }, [isDropdownShowing]);
 
   return (
     <div>
       <DropdownButton
         onClick={() => setIsDropdownShowing(!isDropdownShowing)}
         data-bs-toggle="dropdown"
-        width={dropdownWidth}
+        ref={dropdownButton}
       >
         {selectedDropdownItems.length === 0
           ? placeholderText
@@ -32,8 +47,9 @@ function Dropdown({
           dropdownItems={dropdownItems}
           selectedDropdownItems={selectedDropdownItems}
           setSelectedDropdownItems={setSelectedDropdownItems}
-          dropdownWidth={dropdownWidth}
           direction={direction}
+          uniqueIdentifier={uniqueIdentifier}
+          dropdownButton={dropdownButton}
         />
       )}
     </div>
@@ -47,23 +63,24 @@ const DropdownButton = styled.button`
   border-radius: 0.5rem;
   display: flex;
   align-items: center;
-  width: ${(props) => props.width};
+  width: auto;
+  :focus-within {
+    outline: 3.5px solid #53a8d295;
+  }
 `;
 
 const DownCaret = ({ isDropdownShowing }) => {
   return (
-    <span>
-      <DownCaretSvg
-        height="16"
-        width="16"
-        viewBox="0 0 512 512"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="currentColor"
-        isDropdownShowing={isDropdownShowing}
-      >
-        <path d="M98,190.06,237.78,353.18a24,24,0,0,0,36.44,0L414,190.06c13.34-15.57,2.28-39.62-18.22-39.62H116.18C95.68,150.44,84.62,174.49,98,190.06Z" />
-      </DownCaretSvg>
-    </span>
+    <DownCaretSvg
+      height="16"
+      width="16"
+      viewBox="0 0 512 512"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      isDropdownShowing={isDropdownShowing}
+    >
+      <path d="M98,190.06,237.78,353.18a24,24,0,0,0,36.44,0L414,190.06c13.34-15.57,2.28-39.62-18.22-39.62H116.18C95.68,150.44,84.62,174.49,98,190.06Z" />
+    </DownCaretSvg>
   );
 };
 
@@ -72,6 +89,16 @@ const DownCaretSvg = styled.svg`
   transform: rotateZ(
     ${(props) => (props.isDropdownShowing ? "180deg" : "0deg")}
   );
+  transition: transform 200ms ease-in-out;
 `;
+
+function uuidv4() {
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16)
+  );
+}
 
 export default Dropdown;
--- src/components/DropdownMenu.jsx
+++ src/components/DropdownMenu.jsx
@@ -1,29 +1,88 @@
 import styled from "styled-components";
+import { useEffect, useState } from "react";
 
 function DropdownMenu({
   dropdownItems = [],
   selectedDropdownItems = [],
   setSelectedDropdownItems,
-  dropdownWidth = "17rem",
+  dropdownButton = null,
   direction = "down",
+  multiSelect = false,
+  uniqueIdentifier = "",
 }) {
-  return (
-    <Menu width={dropdownWidth}>
-      <li>test content 1</li>
-      <li>test content 2</li>
-      <li>test content 3</li>
-    </Menu>
-  );
+  const [menuWidth, setMenuWidth] = useState("15rem");
+
+  useEffect(() => {
+    if (dropdownButton) {
+      setMenuWidth(dropdownButton.current.clientWidth + "px");
+    }
+  }, [dropdownButton, setMenuWidth]);
+
+  const menuItems = dropdownItems.map((obj) => {
+    return (
+      <MenuItem
+        onKeyDown={(e) => handleMenuKeyDown(e.key, obj.id, uniqueIdentifier)}
+        key={`dropdown-item-${uniqueIdentifier}-${obj.id}`}
+      >
+        <MenuButton id={`dropdown-button-${uniqueIdentifier}-${obj.id}`}>
+          {obj.text}
+        </MenuButton>
+      </MenuItem>
+    );
+  });
+
+  return <Menu width={menuWidth}>{menuItems}</Menu>;
 }
 
 const Menu = styled.ul`
   width: ${(props) => props.width};
-  background-color: #e3e3e3;
+  background-color: #eeeeee;
   position: absolute;
   list-style-type: none;
-  padding: 0.5rem;
+  padding: 0.75rem 0.5rem;
   border-radius: 0.5rem;
   transform: translateY(0.15rem);
+  box-shadow: 10px 10px 88px -37px rgba(0, 0, 0, 0.48);
 `;
+
+const MenuItem = styled.li``;
+
+const MenuButton = styled.button`
+  border: 0;
+  background: transparent;
+  padding: 0.5rem 0.65rem;
+  border-radius: 0.5rem;
+  width: 100%;
+  text-align: left;
+  transition: background-color 100ms ease-in-out;
+  position: relative;
+  :focus-within {
+    outline: 3.5px solid #53a8d295;
+    z-index: 2;
+  }
+
+  :hover {
+    background-color: #e3e3e3;
+    transition: background-color 100ms ease-in-out;
+  }
+`;
+
+function handleMenuKeyDown(
+  keyCode = "",
+  currentIndex = 0,
+  uniqueIdentifier = ""
+) {
+  if (keyCode === "ArrowUp") {
+    const previousElement = document.getElementById(
+      `dropdown-button-${uniqueIdentifier}-${currentIndex - 1}`
+    );
+    if (previousElement) previousElement.focus();
+  } else if (keyCode === "ArrowDown") {
+    const nextElement = document.getElementById(
+      `dropdown-button-${uniqueIdentifier}-${currentIndex + 1}`
+    );
+    if (nextElement) nextElement.focus();
+  }
+}
 
 export default DropdownMenu;
